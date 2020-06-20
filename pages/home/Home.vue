8a9ad7118b3581c6c1a8ae6c09e0a8212541b77c
kiko233 <1248557135@qq.com>
2020-06-20 22:19:19 +0800

    添加首页

---
+++ pages/home/Home.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+		这里是首页
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'Home',
+		data() {
+			return {
+				title: ''
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
