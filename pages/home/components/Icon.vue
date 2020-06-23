a01ad4e9711748de5f9a2b68d64e7d137aa5859d
kiko233 <1248557135@qq.com>
2020-06-23 19:43:03 +0800

    修改home

--- pages/home/components/Icon.vue
+++ pages/home/components/Icon.vue
@@ -2,10 +2,10 @@
 	<view>
 		<div class="icon">
 			<div class="item"><span class="iconfont">&#xe61a;</span></div>
-			<div class="item">123</div>
-			<div class="item">123</div>
-			<div class="item">123</div>
-			<div class="item">123</div>
+			<div class="item"><span class="iconfont">&#xe61b;</span></div>
+			<div class="item"><span class="iconfont">&#xe61d;</span></div>
+			<div class="item"><span class="iconfont">&#xe602;</span></div>
+			<div class="item"><span class="iconfont">&#xe607;</span></div>
 		</div>
 	</view>
 </template>
@@ -25,5 +25,13 @@
 </script>
 
 <style>
-
+	.icon{
+		display: flex;
+		flex-direction: row;
+		color: #fff;
+	}
+	.iconfont{
+		font-size: 30px;
+		margin: 20px 10px;
+	}
 </style>
