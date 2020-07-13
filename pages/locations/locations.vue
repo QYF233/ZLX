50fa9815f7756dd9764a6bfcd24f3228c009265f
kiko <1248557135@qq.com>
2020-07-13 14:55:02 +0800

    新增寻境

--- pages/locations/locations.vue
+++ pages/locations/locations.vue
@@ -1,9 +1,27 @@
 <template>
-	<view>寻境</view>
+	<view>
+		<view class="container">
+			<view class="status_bar">
+				<!-- 这里是状态栏 -->
+			</view>
+			<view class="header">
+				<image class='header-pic' src="http://cdn.kikohk.top/2020/07/13/bf57c742facca.png"></image>
+			</view>
+			<view class="">
+				<text class="text-black text-bold">
+					寻境，填写一个景区的详细信息。，先做一个导航，让用户选择哪个景点
+				</text>
+				
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
 </script>
 
 <style>
+	.header-pic {
+		width: 100%;
+	}
 </style>
