fb8a9f3f8b7982850be3fb743a2ad92719d6405b
kiko <1248557135@qq.com>
2020-08-29 16:00:39 +0800

    寻宝细节

--- pages/treasure/components/Body.vue
+++ pages/treasure/components/Body.vue
@@ -1,28 +1,19 @@
 <template>
-	<!-- <view class="box">
-		<view class="TreasureImage" v-for="(item,index) in info" :key = 'index'>
-			<view class="item">
-				<view class="url">
-					<image :src="item.url"></image>
-				</view>
-				<view class="name">{{item.name}}</view>
-			</view>
-		</view>
-	</view> -->
-	<view class="box">
-		<view class="bg-img bg-mask flex align-center pic" v-bind:style="{ 'background-image': 'url(' + item.url + ')'}"
-		v-for="(item,index) in info" :key = 'index'>
-			<view class="padding-xl text-white">
-				<view class="padding-xs text-xxl text-bold">
-					{{item.name}}
-				</view>
-				<view class="padding-xs text-lg">
-					{{item.tip}}
+	<view>
+		<view class="box">
+			<view class="bg-img bg-mask flex align-center pic" v-bind:style="{ 'background-image': 'url(' + item.url + ')'}"
+			 v-for="(item,index) in info" :key='index' @tap="onClick(item.id)">
+				<view class="padding-xl text-white">
+					<view class="padding-xs text-xxl text-bold">
+						{{item.name}}
+					</view>
+					<view class="padding-xs text-lg text-Abc">
+						{{item.tip}}
+					</view>
 				</view>
 			</view>
 		</view>
 	</view>
-
 </template>
 
 <script>
@@ -31,34 +22,48 @@
 		data() {
 			return {
 				info: [{
+						id: 1,
 						name: '博物馆',
 						url: require('../../../static/tsImage/1.jpg'),
-						tip:'museum'
+						tip: 'museum'
 					}, {
+						id: 2,
 						name: '科技馆',
 						url: require('../../../static/tsImage/2.jpg'),
-						tip:'Science and Technology Museum'
+						tip: 'Science and Technology Museum'
 					}, {
+						id: 3,
 						name: '展览馆',
 						url: require('../../../static/tsImage/3.jpg'),
-						tip:'Exhibition Hall'
+						tip: 'Exhibition Hall'
 					},
 					{
+						id: 4,
 						name: '纪念馆',
 						url: require('../../../static/tsImage/4.jpg'),
-						tip:'Memorial Hall'
+						tip: 'Memorial Hall'
 					},
 					{
+						id: 5,
 						name: '名人故居',
 						url: require('../../../static/tsImage/5.jpg'),
-						tip:'Former residence of celebrities'
+						tip: 'Former residence of celebrities'
 					},
 					{
+						id: 6,
 						name: '手工艺品',
 						url: require('../../../static/tsImage/6.jpg'),
-						tip:'Handicrafts'
+						tip: 'Handicrafts'
 					}
 				],
+			}
+		},
+		methods: {
+			onClick($event) {
+				console.log($event);
+				uni.navigateTo({
+					url:"/pages/treasure/detail?id="+$event,
+				})
 			}
 		}
 	}
@@ -68,8 +73,9 @@
 	.box {
 		/* margin-top: 10px; */
 	}
-	.pic{
+
+	.pic {
 		height: 414upx;
-		/* margin-top: 20rpx; */
+		/* margin-bottom: 20rpx; */
 	}
 </style>
---
+++ pages/treasure/detail.vue
@@ -0,0 +1,74 @@
+<template>
+	<view class="detail">
+		<view class="item ">
+			<view class="title text-bold">
+				浙江省科技馆
+			</view>
+			<image class="pic" src="../../static/tsImage/1.jpg" mode=""></image>
+			<view class="place text-gray text-ABC">
+				杭州市下城区西湖文化广场A区
+			</view>
+		</view>
+		<view class="item ">
+			<view class="title text-bold">
+				浙江省博物馆
+			</view>
+			<image class="pic" src="../../static/tsImage/2.jpg" mode=""></image>
+			<view class="place text-gray text-ABC">
+				浙江省杭州市西湖区孤山路25号
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'detail',
+		components: {
+
+		},
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style>
+	
+	.item {
+		/* margin: 20rpx 40rpx; */
+		padding: 20rpx 20rpx;
+		position: relative;
+	}
+
+	.item:after {
+		position: absolute;
+		z-index: 3;
+		right: 0;
+		bottom: 0;
+		left: 0px;
+		height: 1px;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #919191;
+	}
+
+	.title {
+		font-size: 18px;
+		font-weight: 900;
+		color: #333333;
+		line-height: 80rpx;
+		/* padding: 0 16px; */
+	}
+	.pic {
+		width: 100%;
+		height: 180px;
+		border-radius: 8px;
+	}
+	.place {
+		margin: 5px 0;
+	}
+</style>
