7a330a08ef1abaccba94036a22a14c16f89d57c9
Kobayashi <q2052094149@163.com>
2020-09-03 20:56:05 +0800

    tourist/search

--- pages/tourist/components/Search.vue
+++ pages/tourist/components/Search.vue
@@ -17,7 +17,9 @@
 			<view class="option-50">
 				<view class="left">人数</view>
 				<view class="right">
-					<input type="text" class="input">
+					<picker @change="bindNumberChange" :value="index_number" :range="number" class="input">
+						<view class="uni-input">{{number[index_number]}}</view>
+					</picker>
 				</view>
 			</view>
 			<view class="option-50">
@@ -29,10 +31,15 @@
 				</view>
 			</view>
 			<view class="option-50">
-				<view class="left">必经</view>
+				<view class="left">范围</view>
 				<view class="right">
-					<input type="text" class="input">
+					<input type="text" class="special_input">
 				</view>
+				<!-- <view class="right">
+					<picker mode="multiSelector" bindchange="bindMultiPickerChange" bindcolumnchange="bindMultiPickerColumnChange" value="{{multiIndex}}" range="{{multiArray}}">
+					　　<view class="uni-input">{{multiArray[0][multiIndex[0]]}} > {{multiArray[1][multiIndex[1]]}}</view>
+					</picker>
+				</view> -->
 			</view>
 			<view class="option-100">
 				<view class="left">开始时间</view>
@@ -65,11 +72,11 @@
 				</view>
 			</view>
 			<view class="option-100">
-				<view class="button-mini">
-					<button type="primary" form-type="submit" size="mini">开始查询</button>
+				<view class="button button-upperPart">
+					<button style="background: #F37B1D; color: #FFFFFF;" form-type="submit" size="default">&nbsp;开始查询&nbsp;</button>
 				</view>
-				<view class="button-mini">
-					<button type="default" form-type="reset" size="mini">清空表单</button>
+				<view class="button">
+					<button style="background: #EFEFEF; color: #AAAAAA;" form-type="reset" size="default">&nbsp;清空表单&nbsp;</button>
 				</view>
 			</view>
 		</form>
@@ -86,8 +93,10 @@ export default{
 		const currentTime = this.getTime();
 		return {
 			types: ['亲子游', '蜜月游', '团建游', '毕业游'],
+			number: ['1人', '2-4人', '5-10人', '11-20人', '21-30人', '30-50人', '50人以上'],
 			preferences: ['娱乐', '美食', '参观'],
 			index_types: 0,
+			index_number: 0,
 			index_preferences: 0,
 			start_date: currentDate,
 			start_time: currentTime,
@@ -106,6 +115,9 @@ export default{
 	methods: {
 		bindTypesChange: function(e) {
 			this.index_types = e.target.value
+		},
+		bindNumberChange: function(e) {
+			this.index_number = e.target.value
 		},
 		bindPreferencesChange: function(e) {
 			this.index_preferences = e.target.value
@@ -155,6 +167,7 @@ export default{
 		},
 		formReset: function(e) {
 			this.index_types = 0;
+			this.index_number = 0;
 			this.index_preferences = 0;
 			this.start_date = this.end_date = this.getDate();
 			this.start_time = this.end_time = this.getTime();
@@ -165,59 +178,68 @@ export default{
 
 <style lang="stylus" scoped>
 	.content
-		margin 4% 2% 4% 2%;
-		padding 2% 1% 2% 1%
-		background-color #F8F8F8;
-		border-radius 20rpx;
+		margin 20rpx;
+		padding 10rpx;
+		background-color #FFFFFF;
+		border-radius 10rpx;
 		overflow auto;
 		.title
-			margin 1% 1% 2% 1%;
-			padding 0 1% 1% 1%;
-			border-bottom 2rpx solid #C0C0C0;
+			margin 0 20rpx 20rpx 20rpx;
+			padding 10rpx;
+			border-bottom 2rpx solid #F37B1D;
 			.font
 				color #555555;
 				font-size 32rpx;
 		.option-50
 			float left;
 			width 50%
-			margin 1% 0 1% 0;
+			margin 10rpx 0;
 			.left
 				float left;
 				display block;
 				width 35%;
-				height 50rpx;
-				line-height 50rpx;
+				height 60rpx;
+				line-height 60rpx;
 				text-align center;
 			.right
 				.input
 					float left;
-					border 2rpx solid #C0C0C0;
+					border 2rpx solid #F37B1D;
 					border-radius 12rpx;
 					background-color #FFFFFF
 					width 60%;
-					height 46rpx;
-					line-height 46rpx;
+					height 56rpx;
+					line-height 56rpx;
+					text-align center;
+				.special_input
+					float left;
+					border 2rpx solid #F37B1D;
+					border-radius 12rpx;
+					background-color #FFFFFF
+					width 60%+1rpx;
+					height 58rpx;
+					line-height 58rpx;
 					text-align center;
 		.option-100
 			float left;
 			width 100%
-			margin 1% 0 1% 0;
+			margin 10rpx 0;
 			.left
 				float left;
 				display block;
 				width 35%;
-				height 50rpx;
-				line-height 50rpx;
+				height 60rpx;
+				line-height 60rpx;
 				text-align center;
 			.right
 				float left;
-				border 2rpx solid #C0C0C0;
+				border 2rpx solid #F37B1D;
 				border-radius 12rpx;
 				padding 0 2rpx;
 				background-color #FFFFFF
 				width 62%;
-				height 46rpx;
-				line-height 46rpx;
+				height 56rpx;
+				line-height 56rpx;
 				text-align center;
 				.picker_date
 					float left;
@@ -225,7 +247,74 @@ export default{
 				.picker_time
 					float left;
 					width 40%;
-			.button-mini
-				float right;
-				margin-right 2%
+			.button
+				margin 10rpx 20rpx;
+			.button-upperPart
+				margin-bottom 15rpx;
+	// .content
+	// 	margin 4% 2% 4% 2%;
+	// 	padding 2% 1% 2% 1%
+	// 	background-color #FFFFFF;
+	// 	border-radius 20rpx;
+	// 	overflow auto;
+	// 	.title
+	// 		margin 1% 1% 2% 1%;
+	// 		padding 0 1% 1% 1%;
+	// 		border-bottom 2rpx solid #F37B1D;
+	// 		.font
+	// 			color #555555;
+	// 			font-size 32rpx;
+	// 	.option-50
+	// 		float left;
+	// 		width 50%
+	// 		margin 1% 0 1% 0;
+	// 		.left
+	// 			float left;
+	// 			display block;
+	// 			width 35%;
+	// 			height 50rpx;
+	// 			line-height 50rpx;
+	// 			text-align center;
+	// 		.right
+	// 			.input
+	// 				float left;
+	// 				border 2rpx solid #F37B1D;
+	// 				border-radius 12rpx;
+	// 				background-color #FFFFFF
+	// 				width 60%;
+	// 				height 46rpx;
+	// 				line-height 46rpx;
+	// 				text-align center;
+	// 	.option-100
+	// 		float left;
+	// 		width 100%
+	// 		margin 1% 0 1% 0;
+	// 		.left
+	// 			float left;
+	// 			display block;
+	// 			width 35%;
+	// 			height 50rpx;
+	// 			line-height 50rpx;
+	// 			text-align center;
+	// 		.right
+	// 			float left;
+	// 			border 2rpx solid #F37B1D;
+	// 			border-radius 12rpx;
+	// 			padding 0 2rpx;
+	// 			background-color #FFFFFF
+	// 			width 62%;
+	// 			height 46rpx;
+	// 			line-height 46rpx;
+	// 			text-align center;
+	// 			.picker_date
+	// 				float left;
+	// 				width 60%
+	// 			.picker_time
+	// 				float left;
+	// 				width 40%;
+	// 		.button
+	// 			// float right;
+	// 			margin-right 2%;
+	// 		.button-upperPart
+	// 			margin-bottom 2%;
 </style>
