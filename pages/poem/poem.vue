033a71b4fb4521d01de9b3c22d13f2f101412fee
kiko <1248557135@qq.com>
2020-10-10 02:17:57 +0800

    寻诗-地图点位修改

--- pages/poem/poem.vue
+++ pages/poem/poem.vue
@@ -8,7 +8,7 @@
 				<!-- <canvas style="width: 500px; height: 500px;" canvas-id="pointCanvas" id="pointCanvas"></canvas> -->
 
 				<view class="" v-for="(item,index) in locale" :key='index'>
-					<span class="city" id="shaoxing" @click="gotoDetail(item.url)" :style="'top:'+item.top+'px;left:'+item.left+'px;'">
+					<span class="city" id="shaoxing" @click="gotoDetail(item.url)" :style="'top:'+item.top+'%;left:'+item.left+'%;'">
 						<view class="" v-if="item.flag">
 							<view class="cityName">{{item.name}}</view>
 							<view class="circle"></view>
@@ -33,92 +33,92 @@
 				scale: 1,
 				ShopName: "123",
 				locale: [{
-					top: 150,
-					left: 170,
+					top: 31.5,
+					left: 45.2,
 					name: "杭州",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 160,
-					left: 140,
+					top: 33,
+					left: 36,
 					name: "西湖区",
 					url: "../search/searchDetail",
 					flag: true
 				}, {
-					top: 160,
-					left: 180,
+					top: 33.5,
+					left: 47,
 					name: "萧山",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 180,
-					left: 200,
+					top: 35,
+					left: 55,
 					name: "柯桥",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 190,
-					left: 210,
+					top: 37,
+					left: 56,
 					name: "越城",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 215,
-					left: 205,
+					top: 41,
+					left: 55,
 					name: "嵊州",
 					url: "../search/searchDetail",
 					flag: true
 				}, {
-					top: 225,
-					left: 230,
+					top: 43.5,
+					left: 63,
 					name: "新昌",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 250,
-					left: 235,
+					top: 48,
+					left: 64,
 					name: "天台",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 270,
-					left: 245,
+					top: 51.5,
+					left: 66,
 					name: "临海",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 290,
-					left: 260,
+					top: 53,
+					left: 75.5,
 					name: "椒江",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 173,
-					left: 250,
+					top: 35.3,
+					left: 69.5,
 					name: "余姚",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 185,
-					left: 280,
+					top: 38,
+					left: 77.5,
 					name: "宁波",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 155,
-					left: 255,
+					top: 32.2,
+					left: 70,
 					name: "慈溪",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 288,
-					left: 210,
+					top: 53.5,
+					left: 58,
 					name: "仙居",
 					url: "../search/searchDetail",
 					flag: false
 				}, {
-					top: 283,
-					left: 240,
+					top: 55,
+					left: 72,
 					name: "黄岩",
 					url: "../search/searchDetail",
 					flag: false
@@ -149,8 +149,8 @@
 					/* 开始画点 */
 					var lo = this.locale[i];
 					ctx.beginPath();
-					ctx.arc(lo.x, lo.y, 2, 0, 2 * Math.PI);
-					ctx.fillText(lo.name, lo.x + 4, lo.y + 2);
+					ctx.arc(lo.left, lo.top, 2, 0, 2 * Math.PI);
+					ctx.fillText(lo.name, lo.left + 4, lo.top + 2);
 					ctx.closePath();
 					/* 结束画点 */
 					ctx.stroke()
@@ -169,11 +169,12 @@
 
 <style lang="stylus" scoped>
 	.movable-view {
+		position relative;
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		height: 100%;
-		width: 100%;
+		height: 1300rpx;
+		width: 750rpx;
 		text-align: center;
 	}
 
