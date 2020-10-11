abc757d17257e6bd4503c958809a10948eb4af49
Kobayashi <q2052094149@163.com>
2020-10-11 14:39:14 +0800

    123

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import {myRequest} from './common/api.js'
 Vue.prototype.$myRequest = myRequest
 Vue.config.productionTip = false
 Vue.prototype.websiteUrl = 'http://zlx.kikohk.top/'
-// Vue.prototype.websiteUrl = 'http://localhost:8081/'
+// Vue.prototype.websiteUrl = 'http://localhost:8080/'
 // Vue.prototype.websiteUrl = 'http://172.18.18.190:8080/'
 Vue.prototype.contentType = 'application/x-www-form-urlencoded'
 
--- pages/painting/painting.vue
+++ pages/painting/painting.vue
@@ -1,8 +1,5 @@
 <template>
 	<view>
-		<view class="status_bar">
-			<!-- 这里是状态栏 -->
-		</view>
 		<swiperImg 
 		:swiperList="swiperList" 
 		:indicatorDots="true" 
@@ -19,33 +16,27 @@
 		},
 		data() {
 			return {
-				swiperList: [{
-						img: '/static/image/painting1.jpg',
-						title: '星空1',
-						content: '安静笼罩著夏夜巨大的星空,覆盖著整个地球,温柔地无声无息安静笼罩著夏夜巨大的星空,覆盖著整个地球,温柔地无声无息安静笼罩著夏夜巨大的星空,覆盖著整个地球,温柔地无声无息安静笼罩著夏夜巨大的星空,覆盖著整个地球,温柔地无声无息安静笼罩著夏夜巨大的星空,覆盖著整个地球,温柔地无声无息安静笼罩著夏夜巨大的星空,覆盖著整个地球,温柔地无声无息安静笼罩著夏夜巨大的星空,覆盖著整个地球,温柔地无声无息'
-					},
-					{
-						img: '/static/image/painting2.jpg',
-						title: '星空2',
-						content: '星星就像一盏盏小电灯,在空中闪烁'
-					},
-					{
-						img: '/static/image/painting1.jpg',
-						title: '星空3',
-						content: '我在星空的这一端,你在星空的那一端,相隔很远这很远。灿烂的永恒,无尽的遥远,再也无法相见..'
-					}, {
-						img: '/static/image/hangzhou.jpg',
-						title: '星空4',
-						content: '一切都如生命一般,在悄然地发生着变化,就如这黑夜一样,失去了光明,却得到了星空.'
-					},
-				],
+				swiperList: []
 			}
 		},
 		onLoad() {
-			
+			this.getData();
 		},
 		methods: {
-			
+			async getData() {
+				uni.showLoading({
+					title: '加载中'
+				});
+				const res1 = await this.$myRequest({
+					url: 'painting/getlist'
+				})
+				uni.hideLoading();
+				console.log(this.swiperList);
+				for (var i = 0; i < res1.data.list.length; i++) {
+					this.swiperList.push(res1.data.list[i])
+				}
+				console.log(this.swiperList);
+			}
 		}
 	}
 </script>
